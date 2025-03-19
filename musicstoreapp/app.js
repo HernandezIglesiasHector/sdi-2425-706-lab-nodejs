43d8411d90fd3d5237407d27d071a30e9208fca9
HernandezIglesiasHector <hhernandeziglesias@gmail.com>
2025-03-20 00:07:48 +0100

    SDI-2425-706-9.4-Registro de usuario, autenticación y uso de sesión.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -5,16 +5,29 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
 
 let app = express();
+let expressSession = require('express-session');
+app.use(expressSession({
+  secret: 'abcdefg',
+  resave: true,
+  saveUninitialized: true
+}));
+
+let crypto = require('crypto');
 let fileUpload = require('express-fileupload');
 app.use(fileUpload({
   limits: { fileSize: 50 * 1024 * 1024 },
   createParentPath: true
 }));
-app.set('uploadPath', __dirname)
 
+
+
+app.set('uploadPath', __dirname)
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,13 +45,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 const { MongoClient } = require("mongodb");
 const connectionStrings = 'mongodb+srv://admin:ADMSIS123...@musicapp.4zvne.mongodb.net/?retryWrites=true&w=majority&appName=musicapp';
 const dbClient = new MongoClient(connectionStrings);
+
+
+const usersRepository = require("./repositories/usersRepository.js");
+usersRepository.init(app, dbClient);
+require("./routes/users.js")(app, usersRepository);
+
+
 let songsRepository = require("./repositories/songsRepository.js");
 songsRepository.init(app, dbClient);
+
+
 
 require("./routes/songs.js")(app, songsRepository);
 require("./routes/authors.js")(app);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -24,6 +24,10 @@ module.exports = function (app, songsRepository) {
         res.send(String(response));
     });
     app.get('/songs/add', function (req, res) {
+        if ( req.session.user == null){
+            res.redirect("/shop");
+            return;
+        }
         res.render("songs/add.twig");
     });
 
@@ -43,10 +47,15 @@ module.exports = function (app, songsRepository) {
         res.send(response);
     });
     app.post('/songs/add', function (req, res) {
+        if ( req.session.user == null){
+            res.redirect("/shop");
+            return;
+        }
         let song = {
             title: req.body.title,
             kind: req.body.kind,
-            price: req.body.price
+            price: req.body.price,
+            author: req.session.user
         }
         songsRepository.insertSong(song, function (result) {
             if (result.songId !== null && result.songId !== undefined) {

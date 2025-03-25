fba8120196a08599e80f2e76bcb66836ee007818
HernandezIglesiasHector <hhernandeziglesias@gmail.com>
2025-03-25 12:42:13 +0100

    SDI-2425-706-10.4-Sistema de compras.

--- musicstoreapp/repositories/songsRepository.js
+++ musicstoreapp/repositories/songsRepository.js
@@ -6,6 +6,16 @@ module.exports = {
     init: function (app, dbClient) {
         this.dbClient = dbClient;
         this.app = app;
+    }, buySong: async function (shop) {
+        try {
+            await this.dbClient.connect();
+            const database = this.dbClient.db(this.database);
+            const purchasesCollection = database.collection('purchases');
+            const result = await purchasesCollection.insertOne(shop);
+            return result;
+        } catch (error) {
+            throw (error);
+        }
     },
     deleteSong: async function (filter, options) {
         try {
@@ -13,6 +23,16 @@ module.exports = {
             const database = this.dbClient.db(this.database);
             const songsCollection = database.collection(this.collectionName);  const result = await songsCollection.deleteOne(filter, options);
             return result;
+        } catch (error) {
+            throw (error);
+        }
+    },getPurchases: async function (filter, options) {
+        try {
+            await this.dbClient.connect();
+            const database = this.dbClient.db(this.database);
+            const purchasesCollection = database.collection('purchases');
+            const purchases = await purchasesCollection.find(filter, options).toArray();
+            return purchases;
         } catch (error) {
             throw (error);
         }
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -61,6 +61,22 @@ module.exports = function (app, songsRepository) {
             res.send("Se ha producido un error al intentar eliminar la canción: " + error)
         });
     })
+    app.post('/songs/buy/:id', function (req, res) {
+        let songId = new ObjectId(req.params.id);
+        let shop = {
+            user: req.session.user,
+            song_id: songId
+        }
+        songsRepository.buySong(shop).then(result => {
+            if (result.insertedId === null || typeof (result.insertedId) === undefined) {
+                res.send("Se ha producido un error al comprar la canción")
+            } else {
+                res.redirect("/purchases");
+            }
+        }).catch(error => {
+            res.send("Se ha producido un error al comprar la canción " + error)
+        })
+    });
 
     app.get('/songs/:kind/:id', function(req, res) {
         let response = 'id: ' + req.params.id + '<br>'
@@ -114,6 +130,23 @@ module.exports = function (app, songsRepository) {
             res.send("Se ha producido un error al listar las canciones " + error)
         });
     });
+    app.get('/purchases', function (req, res) {
+        let filter = {user: req.session.user};
+        let options = {projection: {_id: 0, song_id: 1}};
+        songsRepository.getPurchases(filter, options).then(purchasedIds => {
+            const purchasedSongs = purchasedIds.map(song => song.song_id);
+            let filter = {"_id": {$in: purchasedSongs}};
+            let options = {sort: {title: 1}};
+            songsRepository.getSongs(filter, options).then(songs => {
+                res.render("purchase.twig", {songs: songs});
+            }).catch(error => {
+                res.send("Se ha producido un error al listar las publicaciones del usuario: " + error)
+            });
+        }).catch(error => {
+            res.send("Se ha producido un error al listar las canciones del usuario " + error)
+        });
+    })
+
 
     app.get('/promo*', function (req, res) {
         res.send('Respuesta al patrón promo*');

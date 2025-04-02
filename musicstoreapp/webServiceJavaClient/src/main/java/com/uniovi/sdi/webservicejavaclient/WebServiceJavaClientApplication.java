feea76f7aa40143b3ee46e0fb511e04bfa62586e
HernandezIglesiasHector <uo296095@uniovi.es>
2025-04-02 21:35:55 +0200

    SDI-2425-706-11.7-SW Cliente Java.

---
+++ musicstoreapp/webServiceJavaClient/src/main/java/com/uniovi/sdi/webservicejavaclient/WebServiceJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.webservicejavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceJavaClientApplication.class, args);
+    }
+
+}
---
+++ musicstoreapp/webServiceJavaClient/src/test/java/com/uniovi/sdi/webservicejavaclient/WebServiceJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.webservicejavaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceJavaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
